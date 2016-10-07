303ee4e021ab2ea5001ca0fca05f86754dddba2e
cristian.ordonez <cristian.ordonez@SBBOGLAPPROCO4.sophos.col.com>
2016-10-07 18:32:37 -0500

    se agregan dml y ddl

--- src/main/webapp/sql/DDL.sql
+++ src/main/webapp/sql/DDL.sql
@@ -20,3 +20,5 @@ alter table marketplace.archivo drop column ruta;
 alter table marketplace.parque  change id_parque id_parque  int AUTO_INCREMENT;
 /** se agrega autoincrement a tabla archivo*/
 alter table marketplace.archivo modify column id_archivo int auto_increment;
+
+
